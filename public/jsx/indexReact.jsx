e148d23efcfca37ab7762fbb59ef12acff63c110
qi.chen5 <qi.chen5@renren-inc.com>
2016-01-05 13:42:44 +0800

    css改为react内置方式

--- public/jsx/indexReact.jsx
+++ public/jsx/indexReact.jsx
@@ -5,10 +5,22 @@ var arr = [];
 
 var Comment = React.createClass({
     render: function() {
+        var css_comment = {
+            marginBottom:20
+        }
+        var css_auth = {
+            background: '#a8cc45',
+            color: '#805f63',
+            fontSize:14,
+            textIndent:8,
+            height:25,
+            lineHeight:'25px',
+            marginBottom:10
+        }
         return (
-            <div className="comment">
-                <div className="commentAuthor">{this.props.author}</div>
-                <div className="tip">{this.props.children}</div>
+            <div style={css_comment}>
+                <div style={css_auth}>{this.props.author}</div>
+                <div>{this.props.children}</div>
             </div>
         );
     }
@@ -24,8 +36,11 @@ var CommentList = React.createClass({
                 </Comment>
             );
         });
+        var css_com = {
+            marginTop:8
+        }
         return (
-            <div className="commentList">
+            <div style={css_com}>
                 {commentNodes}
             </div>
         );
@@ -66,12 +81,42 @@ var CommentForm = React.createClass({
         });
     },
     render: function() {
+        var css_form = {
+            width:400,
+            margin:'auto'
+        }
+        var css_tit = {
+            background: '#a5cc8c',
+            height:30,
+            lineHeight:"30px",
+            fontSize:18,
+            textIndent: 8,
+            marginBottom:8
+        }
+        var css_say = {
+            marginTop:8,
+            display: 'block',
+            width:400,
+            height:200
+        }
+        var css_post = {
+            display:'block',
+            margin:'8px auto 0',
+            width:150,
+            height:30,
+            lineHeight:"30px",
+            textAlign:'center',
+            textDecoration: 'none',
+            color:'#000',
+            background: '#a5cc8c',
+            borderRadius: 5
+        }
         return (
-            <div className="commentForm" ref="forms">
-                <div className="commentTitle">发表评论</div>
-                <label for="">用户名：</label><input className="username" type="text" placeholder="Your name" ref="author"/>
-                <textarea className="saySome" type="text" placeholder="Say something..." ref="text"></textarea>
-                <a href="javascript:;" className="postSays" ref="post" onClick={this.handleClick}>提交</a>
+            <div ref="forms" style={css_form}>
+                <div style={css_tit}>发表评论</div>
+                <label for="">用户名：</label><input type="text" placeholder="Your name" ref="author"/>
+                <textarea style={css_say} type="text" placeholder="Say something..." ref="text"></textarea>
+                <a href="javascript:;" style={css_post} ref="post" onClick={this.handleClick}>提交</a>
             </div>
         );
     }
@@ -105,9 +150,21 @@ var CommentBox = React.createClass({
         //setInterval(,40000000000);
     },
     render: function() {
+        var css_com = {
+            width:980,
+            background: '#e5dfe5',
+            margin:'20px auto'
+        }
+        var css_title = {
+            background: '#a5cc8c',
+            height:30,
+            lineHeight:"30px",
+            fontSize:18,
+            textIndent: 8
+        }
         return (
-            <div className="commentBox" ref="commentbox">
-                <div className="chartTitle">评论列表</div>
+            <div ref="commentbox" style={css_com}>
+                <div style={css_title}>评论列表</div>
                 <CommentList data={this.state.data} />
                 <CommentForm />
             </div>
