ec72b275533b2e957c9adccb5de18362bfb2b814
qi.chen5 <qi.chen5@renren-inc.com>
2016-03-11 11:43:01 +0800

    加多说评论框

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,21 @@
  * Created by WesternRanger on 16/2/4.
  */
 define('index',['jquery'],function($){
+    function DOM(){
+        this.wrap = $(".content");
+    }
+    $.extend(DOM.prototype,{
+        init:function(){
+            this.getEvery();
+        },
+        getEvery:function(){
+            this.wrap.on("click","li",function(){
+              console.log(this.nodeName);
+            })
+        }
+    })
+    new DOM().init()//dom操作
+
     var Index = {
         init:function(){
             Index.slide()
