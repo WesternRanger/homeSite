73047b7a3f289315393ce208e731da0dfd74254b
chenqi <qi.chen5@renren-inc.com>
2016-07-17 20:29:58 +0800

    首页侧栏获取数据方式修改

--- public/es6/index.js
+++ public/es6/index.js
@@ -1,110 +1,92 @@
 /**
  * Created by WesternRanger on 16/2/4.
  */
-var siteItems = [];//图推荐站点缓存
 
-$("#swiper li").removeClass("cs_hide");
-if($("#swiper li").length > 1){
-    new Swiper('.swiper .swiper-container', {
-        pagination: '.swiper .swiper-pagination',
-        paginationElement: "li",
-        bulletClass: "test",
-        bulletActiveClass: "on",
-        spaceBetween: 0,
-        centeredSlides: true,
-        autoplay: 3000,
-        loop: true,
-        autoplayDisableOnInteraction: false,
-        onTransitionStart: function () {
-            $('#swiper li img').css({
-                'transform': 'scale(1.05,1.05)',
-                '-webkit-transform': 'scale(1.05,1.05)',
-            });
-        },
-        onTransitionEnd: function () {
-            $('#swiper li img').css({
-                'transform': 'scale(1,1)',
-                '-webkit-transform': 'scale(1,1)',
-            });
-        }
-    });
-}else{
-    $("#swiper .hd").remove();
-}
+class Index{
 
+    constructor(){
+        this.$el = null;
+        this.render();
+        this.slide();
+    }
 
-// 推荐文章跳转
-$('.publish-list').on('click','.new-item',function(){
-    var se = $(this);
-    window.location.href = se.attr('data-url');
-});
+    render(){
+        $.ajax({
+            url:"/api/pushInfo/list",
+            type:'post',
+            success:function(j){
+                let _data = {
+                    intro:[],
+                    site:[],
+                    blog:[],
+                    music:[]
+                };
 
-
-let _url = '/api/pushInfo/list',
-    _data = {
-        //name: 'Hubot',
-        //login: 'hubot',
-    };
-
-// 获取推荐列表
-fetchData(_url,_data,function(j){
-    let _html_intro = '',
-        //_html_site = '',
-        _html_blog = '',
-        _html_music = '';
-
-    j.res.forEach(function(item){
-        if(item.ctype == 'intro'){
-            if(item.url){
-                _html_intro += `<li>
-                <span class="intro-tip">${item.title.split('-')[0]}:</span>
-                <a target="_blank" href="${item.url}">${item.title.split('-')[1]}</a>
-                </li>`;
-            }else{
-                _html_intro += `<li>
-                <span class="intro-tip">${item.title.split('-')[0]}:</span>
-                <span>${item.title.split('-')[1]}</span></li>`;
+                j.res.forEach(function(item){
+                    switch(item.ctype){
+                        case 'intro':
+                            _data.intro.push(item);
+                            break;
+                        case 'site':
+                            _data.site.push(item);
+                            break;
+                        case 'blog':
+                            _data.blog.push(item);
+                            break;
+                        case 'music':
+                            _data.music.push(item);
+                            break;
+                    }
+                });
+                new Vue({
+                    el:'body',
+                    data:_data
+                });
             }
+        });
+        this.loaded();
+    }
 
-        }
-        if(item.ctype == 'site'){
-            //_html_site += `<li><a target="_blank" href="${item.url}">${item.title}</a></li>`;
-            siteItems.push(item);
-        }
+    loaded(){
+        this.$el = $('.publish-list');
+        // 推荐文章跳转
+        this.$el.on('click','.new-item',function(){
+            var se = $(this);
+            window.location.href = se.attr('data-url');
+        });
+    }
 
-        if(item.ctype == 'blog'){
-            _html_blog += `<li><a target="_blank" href="${item.url}">${item.title}</a></li>`;
+    slide(){
+        $("#swiper li").removeClass("cs_hide");
+        if($("#swiper li").length > 1){
+            new Swiper('.swiper .swiper-container', {
+                pagination: '.swiper .swiper-pagination',
+                paginationElement: "li",
+                bulletClass: "test",
+                bulletActiveClass: "on",
+                spaceBetween: 0,
+                centeredSlides: true,
+                autoplay: 3000,
+                loop: true,
+                autoplayDisableOnInteraction: false,
+                onTransitionStart: function () {
+                    $('#swiper li img').css({
+                        'transform': 'scale(1.05,1.05)',
+                        '-webkit-transform': 'scale(1.05,1.05)',
+                    });
+                },
+                onTransitionEnd: function () {
+                    $('#swiper li img').css({
+                        'transform': 'scale(1,1)',
+                        '-webkit-transform': 'scale(1,1)',
+                    });
+                }
+            });
+        }else{
+            $("#swiper .hd").remove();
         }
-        if(item.ctype == 'music'){
-            _html_music += `<li>
-                          <span class="intro-tip">推荐</span>
-                          <a target="_blank" href="${item.url}">${item.title}</a>
-                      </li>`;
-        }
-    });
-    debugger;
-    document.querySelector(".item-block#intro ul").innerHTML = _html_intro;
-    //document.querySelector(".item-block#site ul").innerHTML = _html_site;
-    var site = new Vue({
-        el:'#site',
-        data:{
-            items:[
-                {title:333,url:'www'},
-                {title:444,url:'http'}
-            ]
-        }
-    });
-    document.querySelector(".item-block#blog ul").innerHTML = _html_blog;
-    document.querySelector(".item-block#music ul").innerHTML = _html_music;
-});
-
-function fetchData(_url,_data,func){
-    $.ajax({
-        url:_url,
-        data:_data,
-        type:'post',
-        success:function(j){
-            func(j);
-        }
-    });
+    }
 }
+
+new Index();
+
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,101 +1,107 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
  * Created by WesternRanger on 16/2/4.
  */
-var siteItems = []; //图推荐站点缓存
 
-$("#swiper li").removeClass("cs_hide");
-if ($("#swiper li").length > 1) {
-    new Swiper('.swiper .swiper-container', {
-        pagination: '.swiper .swiper-pagination',
-        paginationElement: "li",
-        bulletClass: "test",
-        bulletActiveClass: "on",
-        spaceBetween: 0,
-        centeredSlides: true,
-        autoplay: 3000,
-        loop: true,
-        autoplayDisableOnInteraction: false,
-        onTransitionStart: function onTransitionStart() {
-            $('#swiper li img').css({
-                'transform': 'scale(1.05,1.05)',
-                '-webkit-transform': 'scale(1.05,1.05)'
+var Index = function () {
+    function Index() {
+        _classCallCheck(this, Index);
+
+        this.$el = null;
+        this.render();
+        this.slide();
+    }
+
+    _createClass(Index, [{
+        key: 'render',
+        value: function render() {
+            $.ajax({
+                url: "/api/pushInfo/list",
+                type: 'post',
+                success: function success(j) {
+                    var _data = {
+                        intro: [],
+                        site: [],
+                        blog: [],
+                        music: []
+                    };
+
+                    j.res.forEach(function (item) {
+                        switch (item.ctype) {
+                            case 'intro':
+                                _data.intro.push(item);
+                                break;
+                            case 'site':
+                                _data.site.push(item);
+                                break;
+                            case 'blog':
+                                _data.blog.push(item);
+                                break;
+                            case 'music':
+                                _data.music.push(item);
+                                break;
+                        }
+                    });
+                    new Vue({
+                        el: 'body',
+                        data: _data
+                    });
+                }
             });
-        },
-        onTransitionEnd: function onTransitionEnd() {
-            $('#swiper li img').css({
-                'transform': 'scale(1,1)',
-                '-webkit-transform': 'scale(1,1)'
+            this.loaded();
+        }
+    }, {
+        key: 'loaded',
+        value: function loaded() {
+            this.$el = $('.publish-list');
+            // 推荐文章跳转
+            this.$el.on('click', '.new-item', function () {
+                var se = $(this);
+                window.location.href = se.attr('data-url');
             });
         }
-    });
-} else {
-    $("#swiper .hd").remove();
-}
-
-// 推荐文章跳转
-$('.publish-list').on('click', '.new-item', function () {
-    var se = $(this);
-    window.location.href = se.attr('data-url');
-});
-
-var _url = '/api/pushInfo/list',
-    _data = {
-    //name: 'Hubot',
-    //login: 'hubot',
-};
-
-// 获取推荐列表
-fetchData(_url, _data, function (j) {
-    var _html_intro = '',
-
-    //_html_site = '',
-    _html_blog = '',
-        _html_music = '';
-
-    j.res.forEach(function (item) {
-        if (item.ctype == 'intro') {
-            if (item.url) {
-                _html_intro += "<li>\n                <span class=\"intro-tip\">" + item.title.split('-')[0] + ":</span>\n                <a target=\"_blank\" href=\"" + item.url + "\">" + item.title.split('-')[1] + "</a>\n                </li>";
+    }, {
+        key: 'slide',
+        value: function slide() {
+            $("#swiper li").removeClass("cs_hide");
+            if ($("#swiper li").length > 1) {
+                new Swiper('.swiper .swiper-container', {
+                    pagination: '.swiper .swiper-pagination',
+                    paginationElement: "li",
+                    bulletClass: "test",
+                    bulletActiveClass: "on",
+                    spaceBetween: 0,
+                    centeredSlides: true,
+                    autoplay: 3000,
+                    loop: true,
+                    autoplayDisableOnInteraction: false,
+                    onTransitionStart: function onTransitionStart() {
+                        $('#swiper li img').css({
+                            'transform': 'scale(1.05,1.05)',
+                            '-webkit-transform': 'scale(1.05,1.05)'
+                        });
+                    },
+                    onTransitionEnd: function onTransitionEnd() {
+                        $('#swiper li img').css({
+                            'transform': 'scale(1,1)',
+                            '-webkit-transform': 'scale(1,1)'
+                        });
+                    }
+                });
             } else {
-                _html_intro += "<li>\n                <span class=\"intro-tip\">" + item.title.split('-')[0] + ":</span>\n                <span>" + item.title.split('-')[1] + "</span></li>";
+                $("#swiper .hd").remove();
             }
         }
-        if (item.ctype == 'site') {
-            //_html_site += `<li><a target="_blank" href="${item.url}">${item.title}</a></li>`;
-            siteItems.push(item);
-        }
+    }]);
 
-        if (item.ctype == 'blog') {
-            _html_blog += "<li><a target=\"_blank\" href=\"" + item.url + "\">" + item.title + "</a></li>";
-        }
-        if (item.ctype == 'music') {
-            _html_music += "<li>\n                          <span class=\"intro-tip\">推荐</span>\n                          <a target=\"_blank\" href=\"" + item.url + "\">" + item.title + "</a>\n                      </li>";
-        }
-    });
-    debugger;
-    document.querySelector(".item-block#intro ul").innerHTML = _html_intro;
-    //document.querySelector(".item-block#site ul").innerHTML = _html_site;
-    var site = new Vue({
-        el: '#site',
-        data: {
-            items: [{ title: 333, url: 'www' }, { title: 444, url: 'http' }]
-        }
-    });
-    document.querySelector(".item-block#blog ul").innerHTML = _html_blog;
-    document.querySelector(".item-block#music ul").innerHTML = _html_music;
-});
+    return Index;
+}();
 
-function fetchData(_url, _data, func) {
-    $.ajax({
-        url: _url,
-        data: _data,
-        type: 'post',
-        success: function success(j) {
-            func(j);
-        }
-    });
-}
+new Index();
 },{}]},{},[1])
