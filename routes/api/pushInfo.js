370102db3c02d574b16b580118cdca332f10d7e5
chenqi <qi.chen5@renren-inc.com>
2016-11-15 14:52:09 +0800

    跨越请求接口

--- routes/api/pushInfo.js
+++ routes/api/pushInfo.js
@@ -5,20 +5,27 @@ var express = require('express'),
     app     = express(),
     tool    = require('../common/tool');
 
-app.post('/list', function(req, page) {
+app.post('/list', function(req, page, next) {
+    //设置允许跨域访问
     page.header('Access-Control-Allow-Origin', '*');
-    page.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    page.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    //page.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    //page.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+
     tool.pool.getConnection(function(err, connection) {
         var sql = 'select * from pushInfo',
             sql_val = [];
         connection.query(sql, sql_val, function(error, rs) {
-            // page.writeHead(200, {'Access-Control-Allow-origin': 'http://localhost:8888/'});
-            page.json({
-                code:0,
-                res:rs,
-                msg:'success'
-            })
+            if (req.method == 'OPTIONS') {
+                page.json({
+                    code:0,
+                    res:rs,
+                    msg:'success'
+                })
+            }else {
+              next();
+            }
+
         });
         connection.release();
     });
